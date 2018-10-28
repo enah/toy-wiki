c8c3e4d1faaceaea6036dcecbd73c3f6c2476132
enah <ehariyoshi@gmail.com>
2018-10-28 18:34:42 -0400

    added history page

--- wiki.py
+++ wiki.py
@@ -7,8 +7,6 @@ app = Flask(__name__)
 def index():
     conn = sqlite3.connect('wiki.db')
     c = conn.cursor()
-    #c.execute("CREATE TABLE if not exists contents (title TEXT, body TEXT)")
-    #c.execute("INSERT INTO contents (title, body) VALUES (?, ?)", ('Red', 'Color'))
     c.execute("SELECT title, body from contents ORDER BY rowid DESC")
     entry = c.fetchone()
     conn.close()
@@ -20,8 +18,6 @@ def index():
 def edit():
     conn = sqlite3.connect('wiki.db')
     c = conn.cursor()
-    #c.execute("CREATE TABLE if not exists contents (title TEXT, body TEXT)")
-    #c.execute("INSERT INTO contents (title, body) VALUES (?, ?)", ('Red', 'Color'))
     c.execute("SELECT title, body from contents ORDER BY rowid DESC")
     entry = c.fetchone()
     conn.close()
@@ -29,20 +25,26 @@ def edit():
         return render_template("edit.html", title=entry[0], body=entry[1])
     return render_template("edit.html", title="", body="")
 
+@app.route("/history")
+def history():
+    conn = sqlite3.connect('wiki.db')
+    c = conn.cursor()
+    posts = [post for post in c.execute("SELECT title, body from contents ORDER BY rowid DESC")]
+    conn.close()
+    return render_template("history.html", posts=posts)
+
 @app.route("/clear")
 def clear():
-    print("Clear called")
     conn = sqlite3.connect('wiki.db')
     c = conn.cursor()
     c.execute("DROP TABLE contents")
     c.execute("CREATE TABLE if not exists contents (title TEXT, body TEXT)")
     conn.commit()
     conn.close()
-    return render_template("edit.html")
+    return ""
 
 @app.route("/save")
 def save():
-    print("Save called")
     a = request.args
     title = a.get("title")
     body = a.get("body")
@@ -51,7 +53,7 @@ def save():
     c.execute("INSERT INTO contents (title, body) VALUES (?, ?)", (title, body))
     conn.commit()
     conn.close()
-    return render_template("edit.html")
+    return ""
 
 if __name__ == '__main__':
     app.run()
